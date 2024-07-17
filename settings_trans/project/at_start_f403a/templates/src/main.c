7b79e2fcc53aa15d568a964ac3d00f792b560151
Mikalash <89709569+Mikalash@users.noreply.github.com>
2024-07-17 13:51:03 +0300

    tabulation fix

--- settings_trans/project/at_start_f403a/templates/src/main.c
+++ settings_trans/project/at_start_f403a/templates/src/main.c
@@ -28,9 +28,9 @@ void EXINT0_IRQHandler(void)
 //-----------------------------------------------------------------------
 int main(void)
 {
-  system_clock_config();
-  at32_board_init();
-  button_exint_init();
+	system_clock_config();
+	at32_board_init();
+	button_exint_init();
 
 	configurate_settings_trans();
 	
@@ -38,15 +38,15 @@ int main(void)
 	if (settings->rpm == 18000)//test
 		debug_f(33);
 		
-  while(1)
-  {
-    at32_led_toggle(LED2);
-    delay_ms(g_speed * DELAY);
-    at32_led_toggle(LED3);
-    delay_ms(g_speed * DELAY);
-    at32_led_toggle(LED4);
-    delay_ms(g_speed * DELAY);
-  }
+	while(1)
+	{
+		at32_led_toggle(LED2);
+		delay_ms(g_speed * DELAY);
+		at32_led_toggle(LED3);
+		delay_ms(g_speed * DELAY);
+		at32_led_toggle(LED4);
+		delay_ms(g_speed * DELAY);
+	}
 }
 //-----------------------------------------------------------------------
 /**
@@ -56,20 +56,20 @@ int main(void)
   */
 void button_exint_init(void)
 {
-  exint_init_type exint_init_struct;
+	exint_init_type exint_init_struct;
 
-  crm_periph_clock_enable(CRM_IOMUX_PERIPH_CLOCK, TRUE);//?
-  gpio_exint_line_config(GPIO_PORT_SOURCE_GPIOA, GPIO_PINS_SOURCE0);//?
+	crm_periph_clock_enable(CRM_IOMUX_PERIPH_CLOCK, TRUE);//?
+	gpio_exint_line_config(GPIO_PORT_SOURCE_GPIOA, GPIO_PINS_SOURCE0);//?
 
-  exint_default_para_init(&exint_init_struct);
-  exint_init_struct.line_enable = TRUE;
-  exint_init_struct.line_mode = EXINT_LINE_INTERRUPUT;
-  exint_init_struct.line_select = EXINT_LINE_0;
-  exint_init_struct.line_polarity = EXINT_TRIGGER_RISING_EDGE;
-  exint_init(&exint_init_struct);
+	exint_default_para_init(&exint_init_struct);
+	exint_init_struct.line_enable = TRUE;
+	exint_init_struct.line_mode = EXINT_LINE_INTERRUPUT;
+	exint_init_struct.line_select = EXINT_LINE_0;
+	exint_init_struct.line_polarity = EXINT_TRIGGER_RISING_EDGE;
+	exint_init(&exint_init_struct);
 
-  nvic_priority_group_config(NVIC_PRIORITY_GROUP_4);
-  nvic_irq_enable(EXINT0_IRQn, 0, 0);
+	nvic_priority_group_config(NVIC_PRIORITY_GROUP_4);
+	nvic_irq_enable(EXINT0_IRQn, 0, 0);
 }
 
 /**
@@ -79,19 +79,19 @@ void button_exint_init(void)
   */
 void button_isr(void)
 {
-  delay_ms(50);
+	delay_ms(50);
 
-  /* clear interrupt pending bit */
-  exint_flag_clear(EXINT_LINE_0);
+	/* clear interrupt pending bit */
+	exint_flag_clear(EXINT_LINE_0);
 
-  /* check input pin state */
-  if(SET == gpio_input_data_bit_read(USER_BUTTON_PORT, USER_BUTTON_PIN))
-  {
-    if(g_speed == SLOW)
-      g_speed = FAST;
-    else
-      g_speed = SLOW;
-  }
+	/* check input pin state */
+	if(SET == gpio_input_data_bit_read(USER_BUTTON_PORT, USER_BUTTON_PIN))
+	{
+		if(g_speed == SLOW)
+			g_speed = FAST;
+		else
+			g_speed = SLOW;
+	}
 }
 
 void plant_settings(uint32_t n_set, struct settings_str* sett)
